70e36ac5228383641ecfba29b34b454db414ca7e
farbod <farbodfooladi@gmail.com>
2025-02-27 21:14:58 +0000

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -12,7 +12,7 @@ export default function ContactPage() {
     message: "",
   });
 
-  const submitted = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
